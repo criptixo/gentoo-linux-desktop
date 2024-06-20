f5b485ecc60ff3fe410861cd2f506b3ce165319f
criptixo <hossam.aissat1@gmail.com>
2024-06-20 14:59:30 +0100

    updated

--- .config/sway/bar.sh
+++ .config/sway/bar.sh
@@ -1,11 +1,8 @@
 while true
 do
-    date=$(date) 
-    memory=$(free -m | awk 'NR==2{printf "RAM: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }')
-    disk=$(df -h | awk '$NF=="/"{printf "/: %d/%dGB (%s)\n", $3,$2,$5}')
-    raid=$(df -h | awk '$NF=="/raid"{printf "/raid: %d/%dGB (%s)\n", $3,$2,$5}')
+    date=$(date "+%H:%M | %d-%m-%Y")
+    memory=$(free -m | awk 'NR==2{printf "RAM: %s/%sMB", $3,$2,$3*100/$2 }')
     cpu=$(top -bn1 | grep load | awk '{printf "CPU: %.2f\n", $(NF-2)}')
-    uptime=$(uptime -p)
-    echo "$uptime | $memory | $disk | $raid | $cpu | $date"
+    echo "$memory | $cpu | $date |"
     sleep 1
 done 
